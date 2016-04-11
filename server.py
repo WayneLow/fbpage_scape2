e2ac446c3c4036cb692ac095006e2744984023ee
Low Jeng Lam <jenglamlow@gmail.com>
2016-04-11 22:34:25 +0800

    Fixed basedir

--- server.py
+++ server.py
@@ -3,18 +3,20 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+import os
+
 
 app = Flask(__name__)
 # app.config.from_object('config')
 db = SQLAlchemy(app)
 
-basedir = "/home/cheonying/Repos/Wayne/fbpage_scape2/"
+basedir = os.path.dirname(os.path.realpath(__file__))
 conffile = basedir+"emonhub.conf"
 
 
 @app.route('/')
 def index():
-    with open(basedir+"index.html", 'rb') as f:
+    with open(basedir+"/index.html", 'rb') as f:
         content = f.read()
     return content
 
